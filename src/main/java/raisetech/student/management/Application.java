69be93782ebf9ed6b1ce726fc9f10bfd446311ba
osajima <chengdaodaxi@gmail.com>
2024-10-19 13:48:13 +0900

    初回データ

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,23 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		// Local
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/Sparkling")
+	public String hello() {
+		return "Hello,Sparkling";
+	}
+}
+
+//Test
